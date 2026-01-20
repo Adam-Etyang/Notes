18533acff9da95048cfbc1c148c9222270fe49c5
Adam-Etyang <adametyang69@icloud.com>
2026-01-20 23:40:31 +0300

    Routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,8 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("viewNotes", "./viewNotes.tsx"),
+  route("note:id", "./viewNotes.tsx"),
+  route("newNote", "./newNote.tsx"),
+] satisfies RouteConfig;
